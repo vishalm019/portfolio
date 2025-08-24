881a580565a1e39ea0ed2b9c794f990fb661163e
vishal <vishalmukundhan019@gmail.com>
2025-08-24 11:44:21 +0530

    Reinitialising the folder structure

--- Frontend/app.py
+++ Frontend/app.py
@@ -3,3 +3,11 @@ import requests
 import pandas as pd
 
 st.title("My Portfolio dashboard")
+
+res = requests.get("/http://127.0.0.1:5000/get_projects")
+
+if res.status_code == 200:
+    projects = res.json()["details"]
+    df = pd.DataFrame(projects)
+    st.subheader("Project")
+    st.dataframe(df)
